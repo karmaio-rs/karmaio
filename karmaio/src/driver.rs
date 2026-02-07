abd3685c044e82391ea0373d8b9b6a3e0011072b
Tarak Sharma <tarak@taraksharma.com>
2026-02-07 22:14:36 +0530

    driver: Scaffold the common Driver trait
    - Create the common driver trait
    - Will be implented by each event system driver

--- karmaio/src/driver.rs
+++ karmaio/src/driver.rs
@@ -1,2 +1,19 @@
+use std::{io, time::Duration};
+
+use crate::driver::ops::Op;
+
 pub(crate) mod drivers;
 pub(crate) mod ops;
+
+pub(crate) trait Driver {
+    // Wait infinitely and process returned events.
+    fn wait(&self) -> io::Result<usize>;
+    // Wait for specified timeout and process returned events.
+    fn wait_with_duration(&self, duration: Duration) -> io::Result<usize>;
+    // Submit an op to the driver
+    fn submit_op<T>(&mut self, mut data: T) -> io::Result<Op<T>>;
+    // Remove an op from the driver
+    fn remove_op<T>(&mut self, &mut data: T);
+    // Poll an op using the driver
+    fn poll_op<T>(&mut self, &mut op: Op<T>, &mut cx: Context<'_>) -> Poll<T::Output>;
+}
